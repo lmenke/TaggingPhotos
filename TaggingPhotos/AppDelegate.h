423498ea75599d3faecc502df98af5790572b619
LHM <unbounded@LHM-MacBook-Pro.local>
2015-01-06 12:15:28 -0500

    Initial Commit

---
+++ TaggingPhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaggingPhotos
+//
+//  Created by LHM on 1/6/15.
+//  Copyright (c) 2015 LP&E. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaggingPhotos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TaggingPhotos
+//
+//  Created by LHM on 1/6/15.
+//  Copyright (c) 2015 LP&E. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
